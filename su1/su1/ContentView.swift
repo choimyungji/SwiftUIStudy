592a23604520f344f9b446ffc4b660002d49e3af
choimyungji <choimyungji@gmail.com>
2020-07-30 00:08:24 +0900

    add su1 directory

---
+++ su1/su1/ContentView.swift
@@ -0,0 +1,65 @@
+//
+//  ContentView.swift
+//  su1
+//
+//  Created by Myungji Choi on 2020/07/28.
+//  Copyright © 2020 Myungji Choi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let rTarget = Double.random(in: 0..<1)
+    let gTarget = Double.random(in: 0..<1)
+    let bTarget = Double.random(in: 0..<1)
+    
+    @State var rGuess: Double
+    @State var gGuess: Double
+    @State var bGuess: Double
+    
+    var body: some View {
+        VStack {
+            HStack {
+                VStack {
+                    Color(red: rTarget, green: gTarget, blue: bTarget)
+                    Text("Match this color")
+                }
+                VStack {
+                    Color(red: rGuess, green: gGuess, blue: bGuess)
+                    Text("R: \(Int(rGuess*255.0))"
+                        + "  G: \(Int(gGuess*255.0))"
+                        + "  B: \(Int(bGuess*255.0))")
+                }
+            }
+            Button(action: {}) {
+                Text("Hit Me!")
+            }
+            ColorSlider(value: $rGuess, textColor: .red)
+            ColorSlider(value: $gGuess, textColor: .green)
+            ColorSlider(value: $bGuess, textColor: .blue)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView(rGuess: 0.7, gGuess: 0.3, bGuess: 0.6)
+            .previewLayout(.fixed(width: 568, height: 320))
+    }
+}
+
+struct ColorSlider: View {
+    @Binding var value: Double
+    var textColor: Color
+    
+    var body: some View {
+        HStack {
+            Text("0")
+                .foregroundColor(.red)
+            Slider(value: $value)
+            Text("255")
+                .foregroundColor(.red)
+        }
+        .padding(.horizontal)
+    }
+}
